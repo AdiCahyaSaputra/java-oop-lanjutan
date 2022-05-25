bb6b24ff16ee910ad0f6cc090fdbfdca9f9e3d9c
AdiCahyaSaputra <adi.JSONparse@gmail.com>
2022-05-25 20:31:59 +0700

    Static Import

--- app/src/main/java/com/oop/app/StaticApp.java
+++ app/src/main/java/com/oop/app/StaticApp.java
@@ -1,15 +1,20 @@
 package com.oop.app;
 
 import com.oop.data.Application;
-import com.oop.data.Constant;
+// import com.oop.data.Constant; 
+
+// Static import 
+import static com.oop.data.Constant.*;
 import com.oop.data.Country;
 import com.oop.utils.MathUtil;
 
 public class StaticApp {
   public static void main(String[] args) {
     // Memanggil static property 
-    System.out.println(Constant.APPLICATION);
-    System.out.println(Constant.PROGRESS);
+    // System.out.println(Constant.APPLICATION);
+    // System.out.println(Constant.PROGRESS);
+    System.out.println(APPLICATION);
+    System.out.println(PROGRESS);
 
     // Memanggil method static 
     System.out.println(MathUtil.sum(6,9,8,10,7,3));
